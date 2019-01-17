b7bb4b93b7cd05ef6cde31eb6125e2b2dfacdc76
simone sabba (lenovo) <simone.sabba@live.com>
2019-01-17 15:38:55 +0100

    app libs

--- src/Web/TravelApp.WebSPA/wwwroot/travel-app/main.js
+++ src/Web/TravelApp.WebSPA/wwwroot/travel-app/main.js
@@ -214,7 +214,7 @@ Object(_angular_platform_browser_dynamic__WEBPACK_IMPORTED_MODULE_1__["platformB
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\AngularApp\src\main.ts */"./src/main.ts");
+module.exports = __webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\ClientApp\src\main.ts */"./src/main.ts");
 
 
 /***/ })
--- src/Web/TravelApp.WebSPA/wwwroot/travel-app/polyfills.js
+++ src/Web/TravelApp.WebSPA/wwwroot/travel-app/polyfills.js
@@ -5790,8 +5790,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\AngularApp\src\polyfills.ts */"./src/polyfills.ts");
-module.exports = __webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\AngularApp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
+__webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\ClientApp\src\polyfills.ts */"./src/polyfills.ts");
+module.exports = __webpack_require__(/*! C:\Users\Simone\source\repos\TravelApp\src\Web\TravelApp.WebSPA\ClientApp\node_modules\@angular-devkit\build-angular\src\angular-cli-files\models\jit-polyfills.js */"./node_modules/@angular-devkit/build-angular/src/angular-cli-files/models/jit-polyfills.js");
 
 
 /***/ })
